65b68990b84f6be7b23277ffaf2e358117c2c1cd
cduang <42569425+cduang@users.noreply.github.com>
2023-10-03 20:57:12 +0800

    Update kokomi.c

--- kokomi.c
+++ kokomi.c
@@ -18,162 +18,179 @@
 #include <linux/mm.h>
 #include <linux/sched/mm.h>
 #include <net/sock.h>
+
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("DRIVER_AUTHOR");
+MODULE_AUTHOR("Jayne");
+MODULE_DESCRIPTION("一个通过Netlink调用内核模块实现内核级物理内存读取的简单示例");
 MODULE_VERSION("0.1");
 
-#define NETLINK_USER 31
+#define NETLINK_CUSTOM_PROTOCOL 31
 
-struct sock *nl_sk = NULL;
-struct netlink_kernel_cfg cfg;
+struct process_info {
+    pid_t pid; // 进程ID
+    size_t virt_addr; // 虚拟地址
+    size_t len; // 数据长度
+};
 
-static inline unsigned long pte_to_phys(unsigned long virt_addr, unsigned long pte_value) {
-    return (virt_addr & ~PAGE_MASK) | (pte_value & PAGE_MASK);
-}
+struct sock *nl_sk = NULL; // Netlink套接字
 
-static inline struct mm_struct *get_mm_by_pid(pid_t pid)
+struct process_info *process_info_data; // 进程信息数据
+
+// 根据进程ID获取进程的内存管理结构体
+static inline struct mm_struct *get_mm_by_pid(pid_t nr)
 {
-	struct task_struct *task;
+    struct task_struct *task;
 
-	task = pid_task(find_vpid(pid), PIDTYPE_PID);
-	if (!task)
-		return NULL;
-	
-	return get_task_mm(task);
+    task = pid_task(find_vpid(nr), PIDTYPE_PID);
+    if (!task)
+        return NULL;
+
+    return get_task_mm(task);
 }
 
+phys_addr_t translate_linear_address(struct mm_struct* mm, uintptr_t va) {
 
-static size_t get_addr_pte(struct mm_struct *mm, size_t virt_addr, pte_t **pte)
-{
-	pgd_t *pgd;
-	p4d_t *p4d;
-	pud_t *pud;
-	pmd_t *pmd;
-	
-	pgd = pgd_offset(mm, virt_addr);
-	if (pgd_none(*pgd))
-		return 0;
-	
-	p4d = p4d_offset(pgd, virt_addr);
-	if (p4d_none(*p4d))
-		return 0;
-		
-	pud = pud_offset(p4d, virt_addr);
-	if (pud_none(*pud))
-		return 0;
-		
-	pmd = pmd_offset(pud, virt_addr);
-	if (pmd_none(*pmd))
-		return 0;
-	
-	*pte = pte_offset_kernel(pmd, virt_addr);
-	if (pte_none(**pte))
-		return 0;
-	
-	return pte_val(**pte);
+    pgd_t *pgd;
+    p4d_t *p4d;
+    pmd_t *pmd;
+    pte_t *pte;
+    pud_t *pud;
+
+    phys_addr_t page_addr;
+    uintptr_t page_offset;
+
+    pgd = pgd_offset(mm, va);
+    if(pgd_none(*pgd) || pgd_bad(*pgd)) {
+        return 0;
+    }
+    p4d = p4d_offset(pgd, va);
+    if (p4d_none(*p4d) || p4d_bad(*p4d)) {
+        return 0;
+    }
+    pud = pud_offset(p4d,va);
+    if(pud_none(*pud) || pud_bad(*pud)) {
+        return 0;
+    }
+    pmd = pmd_offset(pud,va);
+    if(pmd_none(*pmd)) {
+        return 0;
+    }
+    pte = pte_offset_kernel(pmd,va);
+    if(pte_none(*pte)) {
+        return 0;
+    }
+    if(!pte_present(*pte)) {
+        return 0;
+    }
+    //页物理地址
+    page_addr = (phys_addr_t)(pte_pfn(*pte) << PAGE_SHIFT);
+    //页内偏移
+    page_offset = va & (PAGE_SIZE-1);
+
+    return page_addr + page_offset;
 }
 
+// 接收Netlink消息的回调函数
 static void nl_recv_msg(struct sk_buff *skb) {
     struct nlmsghdr *nlh;
     int pid;
     int send_pid;
     struct sk_buff *skb_out;
-    int msg_size;
-    char *msg;
     int res;
-    unsigned long virt_addr;
-    unsigned long phys_addr;
-    size_t page_addr;
-    void *kernel_addr;
+    size_t virt_addr;
+    phys_addr_t phys_addr;
+    void* kernel_addr;
+    size_t len;
     struct mm_struct *mm;
-    pte_t *pte = NULL;
-    int data;
-
-    char *input;
-    char *virt_addr_str;
-    char *offset_str;
-    char *pid_str;
-    unsigned long offset;
+    int err = 0;
 
     printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
 
     nlh = (struct nlmsghdr *)skb->data;
-    //printk(KERN_INFO "Netlink received msg payload: %s\n", (char *)nlmsg_data(nlh));
 
-    send_pid = nlh->nlmsg_pid; // pid of sending process
+    send_pid = nlh->nlmsg_pid; // 发送方进程的PID
 
+    // 解析虚拟地址和偏移量
+    process_info_data = (struct process_info *)nlmsg_data(nlh);
+    virt_addr = process_info_data->virt_addr;
+    pid = process_info_data->pid;
+    len = process_info_data->len;
 
-    // Parse virtual address and offset
-    
-
-    input = (char *)nlmsg_data(nlh);
-    virt_addr_str = strsep(&input, " ");
-    offset_str = strsep(&input, " ");
-    pid_str = strsep(&input, " ");
-    virt_addr = simple_strtoul(virt_addr_str, NULL, 16);
-    offset = simple_strtoul(offset_str, NULL, 10);
-    pid = simple_strtoul(pid_str, NULL, 10);
-    phys_addr = 0;
-
-    //printk(KERN_ERR "v2p: send pid: %d,pid: %d\n",send_pid,pid);
+    printk(KERN_ERR "v2p: send pid: %d,pid: %d,virt_addr: %lx,len: %ld\n",send_pid,pid,virt_addr,len);
     mm = get_mm_by_pid(pid);
 
-    page_addr = get_addr_pte(mm, virt_addr, &pte);
-
-    //printk(KERN_ERR "v2p: page_addr,%lx\n",page_addr);
-
-    if (mm && pte && pte_present(*pte) && pfn_valid(pte_pfn(*pte))) {
-        //printk(KERN_ERR "v2p: 1,%lx\n",virt_addr);
-        phys_addr = pte_to_phys(virt_addr, pte_val(*pte)) & 0x7fffffffffffffULL;
-        //printk(KERN_ERR "v2p: 2,%lx\n",phys_addr);
-        kernel_addr = __va(phys_addr);
-        data = *(int *)kernel_addr;
-        //printk(KERN_ERR "v2p: 3,%d\n",data);
-    }else{
-        printk(KERN_ERR "v2p: Failed to mm or pte\n");
+    if (!mm) {
+        printk(KERN_ERR "v2p: 获取mm 出错\n");
+        return;
     }
 
-    msg_size = sizeof(data);
+    phys_addr = translate_linear_address(mm,virt_addr);
 
-    skb_out = nlmsg_new(msg_size, 0);
+    mmput(mm); // 移到这里
+
+    if (!phys_addr) {
+        printk(KERN_ERR "v2p: 获取phys_addr 出错\n");
+        return;
+    }
+
+    if (!pfn_valid(__phys_to_pfn(phys_addr))) {
+        return;
+    }
+    kernel_addr = ioremap_cache(phys_addr, len);
+
+    if(!kernel_addr){
+        printk(KERN_ERR "v2p: 获取read_physical_address 出错\n");
+        return;
+    }
+
+    printk(KERN_ERR "v2p: kernel_addr: %d\n",*(int *)kernel_addr);
+
+    skb_out = nlmsg_new(len, 0);
     if (!skb_out) {
-        printk(KERN_ERR "Failed to allocate new skb\n");
-        return;
+        err = -ENOMEM;
+        goto out_free;
     }
 
-    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, len, 0);
     if (!nlh) {
-        printk(KERN_ERR "Failed to create netlink message header\n");
-        nlmsg_free(skb_out);
-        return;
+        err = -ENOMEM;
+        goto out_free_skb;
     }
 
-    memcpy(nlmsg_data(nlh), &data, msg_size);
+    memcpy(nlmsg_data(nlh), kernel_addr, len);
+    iounmap(kernel_addr);
 
     res = nlmsg_unicast(nl_sk, skb_out, send_pid);
     if (res < 0) {
-        printk(KERN_INFO "Error while sending back to user\n");
+        err = res;
+        goto out_free_skb;
     }
+out_free:
+    return;
 
-    kfree(msg);
+out_free_skb:
+    nlmsg_free(skb_out);
 }
 
-
-
+// 模块初始化函数
 static int __init dev_init(void) {
+
     printk(KERN_INFO "Loading netlink_virt_to_phys module...\n");
 
-    cfg.input = nl_recv_msg;
+    struct netlink_kernel_cfg cfg = {
+        .input = nl_recv_msg,
+    }; // Netlink内核配置
 
-    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+    nl_sk = netlink_kernel_create(&init_net, NETLINK_CUSTOM_PROTOCOL, &cfg);
     if (!nl_sk) {
-        printk(KERN_ALERT "Error creating socket.\n");
+        printk(KERN_ALERT "Failed to create netlink socket.\n");
         return -10;
     }
-
+    printk(KERN_INFO "Netlink socket created.\n");
     return 0;
 }
 
+// 模块卸载函数
 static void __exit dev_exit(void) {
     printk(KERN_INFO "Unloading netlink_virt_to_phys module...\n");
 
